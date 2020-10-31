98fb5901d693538b907834c14acaf5fcdb963dd0
mike <mihai97ionita@gmail.com>
2020-10-31 15:47:51 +0200

    first split

--- main_train.py
+++
@@ -1,6 +0,0 @@
-from functions import train
-from functions import load_dataset
-
-print("FOR ALL \n")
-rezultate_finale=train.DT_boosted_fit(load_dataset.x_train, load_dataset.y_train, load_dataset.x_test, load_dataset.y_test, "ALL")
-print(rezultate_finale)
